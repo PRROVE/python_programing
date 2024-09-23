16a7d390bee5339aabb15989c22c6139050348c7
PRROVE <103507963+PRROVE@users.noreply.github.com>
2024-09-23 11:56:40 +0900

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,16 @@
+"""
+a= [11, 22, 33, 44, 55, 66, 77, 88, 99, 100]
+
+a.append([11])
+print(a)
+a.extend([12,13])
+print(a)
+append = 괄호안에 list를 벗겨주지 않고 list에 포함시켜줌
+extend = 괄호안에 list를 벗기고 list에 포함시켜줌
+"""
+
+for i in range(100):
+    a,b= map(int,input().split())
+    if(a==0 and b == 0):
+        break
+    print(a+b)
